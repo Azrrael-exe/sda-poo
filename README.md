5a0487026c6f3aa333452b8a5cdeb347d012744a
vcamargo <vcamargo.e@gmail.com>
2018-07-26 00:45:35 -0500

    02 - Descripción de la Clase  Changes to be committed: 	modified:   README.md 	modified:   src/main.cpp

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sda-poo
+
+Datashet: [MCP4141](http://ww1.microchip.com/downloads/en/DeviceDoc/22059b.pdf)
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+#include <SPI.h>
+
+class MCP4141 {
+    public:
+        MCP4141();
+        MCP4141(uint8_t ce);            //Constructor
+        void setPos(uint8_t pos);
+        uint8_t getPos();
+        void setPercent(uint8_t percent);
+    private:
+        uint8_t chip_enable;
+        uint8_t pos;
+};
 
 void setup() {
     Serial.begin(115200);
