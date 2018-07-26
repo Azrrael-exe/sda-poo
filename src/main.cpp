67abd05d9dd0ea16006ad565e602fa8cc1669583
vcamargo <vcamargo.e@gmail.com>
2018-07-26 00:54:12 -0500

    2.1 - Clase Editada  Changes to be committed: 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@ class MCP4141 {
     public:
         MCP4141();
         MCP4141(uint8_t ce);            //Constructor
-        void setPos(uint8_t pos);
+        uint8_t setPos(uint8_t pos);
         uint8_t getPos();
-        void setPercent(uint8_t percent);
+        uint8_t setPercent(uint8_t percent);
+        uint8_t increment();
+        uint8_t decrement();
     private:
         uint8_t chip_enable;
         uint8_t pos;
