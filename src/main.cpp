4835b52026a43210890b36c732cc80e3f1c93dc0
victor.camargo <victor.camargo@rappi.com>
2022-08-19 21:05:36 -0500

    Circular Buffer
    Circular Buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <circular.h>
+
+CircularBuffer buffer = CircularBuffer(10);
 
 void setup(){
-
+    Serial.begin(115200);
 }
 
-void loop(){
-    
+void loop(){ 
+    for(int i=0; i < 10; i++){
+        buffer.add(i);
+        Serial.println(buffer.freeSlots());
+        delay(1000);
+    }
 }
